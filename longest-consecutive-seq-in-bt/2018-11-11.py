478780a73c65e7e297a99e27801bbcc6cb783c79
Jordan Siaw <jordansiaw.cy@gmail.com>
2018-11-11 10:25:42 -0800

    Update notes on 'longest consecutive seq in BT'

--- longest-consecutive-seq-in-bt/2018-11-11.py
+++ longest-consecutive-seq-in-bt/2018-11-11.py
@@ -3,17 +3,20 @@ https://www.geeksforgeeks.org/longest-consecutive-sequence-binary-tree/
 
 Given a binary tree, find the length of the longest path which comprises of nodes with consecutive values in increasing order. Every node is considered as a path of length 1.
 
+
 - Runs in O(n) time.
 - Uses O(1) space.
 - I found this on Program Creek first (https://www.programcreek.com/2014/04/leetcode-binary-tree-longest-consecutive-sequence-java/), and was confused by the wording of the question - I thought I just had to find the length of the longest root-to-leaf path.
 - I'm not too happy with modifying some global variable (longest), but trying to change that seems to quickly make the code less readable.
 """
 
-class Node:
+
+class BTNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
+
 
 class Solution:
     longest = 0
@@ -43,11 +46,11 @@ class Solution:
         self.helper(root.right, root.val + 1, currLength)
 
     def test(self):
-        r = Node(6)
-        r.right = Node(9)
-        r.right.left = Node(7)
-        r.right.right = Node(10)
-        r.right.right.right = Node(11)
+        r = BTNode(6)
+        r.right = BTNode(9)
+        r.right.left = BTNode(7)
+        r.right.right = BTNode(10)
+        r.right.right.right = BTNode(11)
 
         seq = self.longestConsecutiveSeq(r)
         print(seq)
